3741685176124ff67fe46182c7f7e3c5859ad4bc
Matthzer92 <matthieu.noiret@epita.fr>
2018-03-10 20:11:35 +0100

    Update Matthieu

---
+++ Boosted/Assets/MouseLock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+	}
+	
+	void OnApplicationFocus(bool ApplicationIsBack)
+	{
+		if (ApplicationIsBack == true)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+			
+	}
+
+}
