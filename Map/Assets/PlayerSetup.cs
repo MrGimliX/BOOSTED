756250ae26c81da3d633004b5248ef29e9f568fc
MrGimliX <MrGimliX@users.noreply.github.com>
2018-03-11 18:26:14 +0100

    Added edge object

--- Map/Assets/PlayerSetup.cs
+++ Map/Assets/PlayerSetup.cs
@@ -2,17 +2,32 @@ using UnityEngine.Networking;
 using UnityEngine;
 
 public class PlayerSetup : NetworkBehaviour {
-	
+
 	[SerializeField]
 	Behaviour[] componentsToDisable;
+
+	Camera sceneCamera;
 	void Start ()
 	{
-		if (!isLocalPlayer)
-		{
-			for (int i = 0; i < componentsToDisable.Length; i++)
-			{
+		if (!isLocalPlayer) {
+			for (int i = 0; i < componentsToDisable.Length; i++) {
 				componentsToDisable [i].enabled = false;
 			}
+		} else
+		{
+			sceneCamera = Camera.main;
+			if (sceneCamera != null)
+			{
+				sceneCamera.gameObject.SetActive (false);
+			}
+		}
+	}
+
+	void OnDisable ()
+	{
+		if (sceneCamera != null) 
+		{
+			sceneCamera.gameObject.SetActive (true);
 		}
 	}
 }
