8ee2f8dcc75dc781ed313a4163ebfa3b075111d7
MrGimliX <MrGimliX@users.noreply.github.com>
2018-03-15 17:37:07 +0100

    Added animations for guns and walking

--- Map/Assets/Characteranimator.cs
+++ Map/Assets/Characteranimator.cs
@@ -14,8 +14,9 @@ public class Characteranimator : MonoBehaviour{
 	}
 
 	// Update is called once per frame
-	void Update () {
-		float SpeedPercent = agent.velocity.magnitude / agent.speed;
-		animator.SetFloat ("SpeedPercent", SpeedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	void Update () 
+	{
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat ("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
 	}
 }
--- Map/Assets/GunGlock.cs
+++ Map/Assets/GunGlock.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GunGlock : MonoBehaviour {
 
 	public float damage = 10f;
 	public float range = 100f;
 	public float impforce = 30f;
-	public float fireRate = 15f;
+	public float fireRate = 2f;
+
+	public int maxAmmo = 10;
+	public int currentAmmo;
+	public float reloadTime = 2f;
+	private bool isReloading = false;
 
 	public Camera fpsCam;
 	public ParticleSystem muzzleFlash;
@@ -13,13 +19,41 @@ public class GunGlock : MonoBehaviour {
 
 	private float nextTimeToFire = 0f;
 
+	public Animator animator;
+
+	void Start()
+	{
+		currentAmmo = maxAmmo;
+	}
+
 	void Update () {
-		if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+
+		if (isReloading)
+			return;
+
+		if (currentAmmo <= 0)
+		{
+			StartCoroutine(Reload());
+			return;
+		}
+		if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
 		{
 			nextTimeToFire = Time.time + 1f/fireRate;
 			Shoot();
 		}
 	}
+
+	IEnumerator Reload ()
+	{
+		isReloading = true;
+		animator.SetBool("Reloading", true);
+		yield return new WaitForSeconds(reloadTime - 0.25f);
+		animator.SetBool("Reloading", false);
+		yield return new WaitForSeconds(0.25f);
+		currentAmmo = maxAmmo;
+		isReloading = false;
+	}
+
 	void Shoot()
 	{
 		muzzleFlash.Play();
