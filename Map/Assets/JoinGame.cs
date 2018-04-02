27e7b56846573ad60bd5c38ad7208285bad846f1
MrGimliX <MrGimliX@users.noreply.github.com>
2018-04-02 17:12:10 +0200

    Added lobby

---
+++ Map/Assets/JoinGame.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+
+	List<GameObject> roomList = new List<GameObject>();
+
+	[SerializeField]
+	private Text status;
+
+	[SerializeField]
+	private GameObject roomListPrefab;
+
+	[SerializeField]
+	private Transform roomListParent;
+
+	private NetworkManager networkManager;
+
+	void Start ()
+	{
+		networkManager = NetworkManager.singleton;
+		if (networkManager.matchMaker == null)
+		{
+			networkManager.StartMatchMaker();
+		}
+		Refresh();
+	}
+	public void Refresh()
+	{
+		networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+		status.text = "Loading...";
+	}
+	public void OnMatchList (bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+	{
+		status.text = "";
+
+		if(matchList == null)
+		{
+			status.text = "Couldn't get room list.";
+			return;
+		}
+		ClearRoomList();
+		foreach (MatchInfoSnapshot match in matchList)
+		{
+			GameObject _roomListItemGO = Instantiate(roomListPrefab);
+			_roomListItemGO.transform.SetParent(roomListParent);
+			roomList.Add(_roomListItemGO);
+		}
+		if (roomList.Count == 0)
+		{
+			status.text = "You are the only one playing.";
+		}
+	}
+	void ClearRoomList()
+	{
+		for (int i = 0 ; i < roomList.Count; i++)
+		{
+			Destroy(roomList[i]);
+		}
+		roomList.Clear();
+	}
+
+}
