ae2d82b75666754a80ca5ab118002efb050b2053
Matthzer92 <matthieu.noiret@epita.fr>
2018-05-03 17:09:19 +0200

    joining not workin
    nsm

--- Map/Assets/JoinGame.cs
+++ Map/Assets/JoinGame.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
+using System.Collections;
 
 public class JoinGame : MonoBehaviour {
 
@@ -12,7 +14,7 @@ public class JoinGame : MonoBehaviour {
 	private Text status;
 
 	[SerializeField]
-	private GameObject roomListPrefab;
+	private GameObject roomListItemPrefab;
 
 	[SerializeField]
 	private Transform roomListParent;
@@ -26,55 +28,108 @@ public class JoinGame : MonoBehaviour {
 		{
 			networkManager.StartMatchMaker();
 		}
-		Refresh();
+
+		RefreshRoomList();
 	}
-	public void Refresh()
+
+	public void RefreshRoomList ()
 	{
 		ClearRoomList();
+
+		if (networkManager.matchMaker == null)
+		{
+			networkManager.StartMatchMaker();
+		}
+
 		networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
 		status.text = "Loading...";
 	}
+
 	public void OnMatchList (bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
 	{
 		status.text = "";
 
-		if(matchList == null)
+		if (!success || matchList == null)
 		{
 			status.text = "Couldn't get room list.";
 			return;
 		}
+
 		foreach (MatchInfoSnapshot match in matchList)
 		{
-			GameObject _roomListItemGO = Instantiate(roomListPrefab);
+			GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
 			_roomListItemGO.transform.SetParent(roomListParent);
 
-			ServerList _joingame = _roomListItemGO.GetComponent<ServerList>();
-			if (_joingame != null)
+			RoomList _roomListItem = _roomListItemGO.GetComponent<RoomList>();
+			if (_roomListItem != null)
 			{
-				_joingame.Setup(match, JoinRoom);
+				_roomListItem.Setup(match, JoinRoom);
 			}
-
+			// as well as setting up a callback function that will join the game.
 			roomList.Add(_roomListItemGO);
 		}
+/*
+		int i=0;
+		foreach (MatchInfoSnapshot match in matchList)
+		{
+			GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+			_roomListItemGO.transform.SetParent(roomListParent);
+			GameObject button = getButton(i);
+			button.setText(roomName);
+			button.onClick = match(joinRoom);
+			button.show();
+		}*/
+
+
 		if (roomList.Count == 0)
 		{
-			status.text = "You are the only one playing.";
+			status.text = "No rooms at the moment.";
 		}
 	}
+
 	void ClearRoomList()
 	{
-		for (int i = 0 ; i < roomList.Count; i++)
+		for (int i = 0; i < roomList.Count; i++)
 		{
 			Destroy(roomList[i]);
 		}
+
 		roomList.Clear();
 	}
 
-	public void JoinRoom (MatchInfoSnapshot match)
+	public void JoinRoom (MatchInfoSnapshot _match)
 	{
-		networkManager.matchMaker.JoinMatch(match.networkId, "", "","", 0, 0, networkManager.OnMatchJoined);
+		networkManager.matchMaker.JoinMatch(_match.networkId, "", "", "", 0, 0, networkManager.OnMatchJoined);
+		StartCoroutine(WaitForJoin());
+	}
+
+	IEnumerator WaitForJoin ()
+	{
 		ClearRoomList();
-		status.text = "JOINING...";
+
+		int countdown = 10;
+		while (countdown > 0)
+		{
+			status.text = "JOINING... (" + countdown + ")";
+
+			yield return new WaitForSeconds(1);
+
+			countdown--;
+		}
+
+		// Failed to connect
+		status.text = "Failed to connect.";
+		yield return new WaitForSeconds(1);
+
+		MatchInfo matchInfo = networkManager.matchInfo;
+		if (matchInfo != null)
+		{
+			networkManager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, networkManager.OnDropConnection);
+			networkManager.StopHost();
+		}
+
+		RefreshRoomList();
+
 	}
 
 }
