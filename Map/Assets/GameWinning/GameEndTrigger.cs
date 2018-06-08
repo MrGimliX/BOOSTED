e0f3471a62685af5c22facaa9eb8999dd7c3c57c
Matthzer92 <matthieu.noiret@epita.fr>
2018-06-08 18:10:06 +0200

    Script endgame
    endgame script added dab !!!!!

---
+++ Map/Assets/GameWinning/GameEndTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndTrigger : MonoBehaviour {
+
+	public GameWinningManager gameManager;
+	void OnTriggerEnter()
+	{
+		gameManager.WinGameHuntersEscape();
+	}
+}
---
+++ Map/Assets/GameWinning/GameWinningManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameWinningManager : MonoBehaviour {
+
+	public GameObject CompleteLevelUI;
+
+
+	public void WinGameHuntersEscape()
+	{
+		CompleteLevelUI.SetActive(true);
+	}
+	/*bool GameEnded = false;
+
+	public float RestartDelay = 3f;
+
+	public void EndGame()
+	{
+		if (GameEnded == false)
+		{
+			GameEnded = true;
+			Debug.Log("Hunters Win")
+			Invoke("Restart", RestartDelay)
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene("Lobby")
+	}*/
+}
